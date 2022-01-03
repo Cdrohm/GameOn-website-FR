52ad776812f16e08368bd70d724e96d48712e3b1
Clément Théraud <theraud.clement@gmail.com>
2022-01-03 09:03:45 +0100

    Update modal.js

--- starterOnly/js/modal.js
+++ starterOnly/js/modal.js
@@ -171,7 +171,7 @@ function validateEventCity(eventCity) {
   })
   
   if (eventCityChecked === 0) {
-        //errorEventCity.style.display = "inline";
+        errorEventCity.style.display = "inline";
         errorEventCity.innerText = "Veuillez choisir une ou plusieurs ville(s).";
         errorEventCity.style.color = 'red';
         errorEventCity.style.fontSize = '0.8rem';
