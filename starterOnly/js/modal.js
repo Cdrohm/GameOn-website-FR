2e9f71e8a89c5f06eca60b2b075ff05bdc9ed851
Clément Théraud <theraud.clement@gmail.com>
2022-01-09 21:07:44 +0100

    identifer

--- starterOnly/js/modal.js
+++ starterOnly/js/modal.js
@@ -42,9 +42,9 @@ const confirmationValidation = document.querySelector("#confirm-modal");
 let prev = null;
 let eventCity = null;
 for (var i = 0; i < radios.length; i++) {
-  radios[i].onclick = function () {
-    eventCity = this.value;
-  };
+    radios[i].onclick = function() {
+        eventCity = this.value;
+    };
 }
 
 // elements SEND FORM (envoi formulaire)
@@ -54,12 +54,12 @@ const form = document.querySelector('form[name="reserve"]')
 menuResponsive.addEventListener('click', editNav);
 //Changement du menu selon taille écran / menu 'burger'
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
 
 // launch modal event
@@ -67,23 +67,23 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
-  confirmationValidation.style.display = 'none';
+    modalbg.style.display = "block";
+    confirmationValidation.style.display = 'none';
 }
 
 // Close Modal Form
 closeModalBtn[0].addEventListener("click", closeModal);
 //fonction fermeture du formulaire
 function closeModal() {
-  modalbg.style.display = 'none';
-  form.style.display = 'block';
-  confirmationValidation.style.display = 'none';
+    modalbg.style.display = 'none';
+    form.style.display = 'block';
+    confirmationValidation.style.display = 'none';
 }
 
 // Send Form Registration
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-  validate();
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    validate();
 });
 
 // validate FORM INPUT
@@ -93,20 +93,20 @@ form.addEventListener('submit', function (e) {
  * @returns message d'erreur si chiffres ou -2 caractères
  */
 function validateFirstName(firstName) {
-  //console.log (firstName.value.match(regex),regex.test(firstName));
-  if (firstName.value.trim().length < 2 || first.value.trim() === '' || !firstName.value.match(regex)) {
-    errorFirstName.style.display = "inline";
-    errorFirstName.innerText = "Veuillez entrer 2 caractères ou plus pour le champ du Prénom.";
-    errorFirstName.style.color = 'red';
-    errorFirstName.style.fontSize = '0.8rem';
-    errorFirstName.style.marginTop = '10px';
-    firstName.style.border = 'solid red 2px';
-    return false;
-  } else {
-    errorFirstName.style.display = 'none';
-    firstName.style.border = 'solid #279e7a 3px';
-    return true;
-  };
+    //console.log (firstName.value.match(regex),regex.test(firstName));
+    if (firstName.value.trim().length < 2 || first.value.trim() === '' || !firstName.value.match(regex)) {
+        errorFirstName.style.display = "inline";
+        errorFirstName.innerText = "Veuillez entrer 2 caractères ou plus pour le champ du Prénom.";
+        errorFirstName.style.color = 'red';
+        errorFirstName.style.fontSize = '0.8rem';
+        errorFirstName.style.marginTop = '10px';
+        firstName.style.border = 'solid red 2px';
+        return false;
+    } else {
+        errorFirstName.style.display = 'none';
+        firstName.style.border = 'solid #279e7a 3px';
+        return true;
+    };
 }
 
 /**
@@ -115,19 +115,19 @@ function validateFirstName(firstName) {
  * @returns message d'erreur si chiffres ou -2 caractères
  */
 function validateLastName(lastName) {
-  if (lastName.value.toString().trim().length < 2 || first.value.trim() === '' || !lastName.value.match(regex)) {
-    errorLastName.style.display = 'inline';
-    errorLastName.innerText = "Veuillez entrer 2 caractères ou plus pour le champ du Nom.";
-    errorLastName.style.color = 'red';
-    errorLastName.style.fontSize = '0.8rem';
-    errorLastName.style.marginTop = '10px';
-    lastName.style.border = 'solid red 2px';
-    return false;
-  } else {
-    errorLastName.style.display = 'none';
-    lastName.style.border = 'solid #279e7a 3px';
-    return true;
-  }
+    if (lastName.value.toString().trim().length < 2 || first.value.trim() === '' || !lastName.value.match(regex)) {
+        errorLastName.style.display = 'inline';
+        errorLastName.innerText = "Veuillez entrer 2 caractères ou plus pour le champ du Nom.";
+        errorLastName.style.color = 'red';
+        errorLastName.style.fontSize = '0.8rem';
+        errorLastName.style.marginTop = '10px';
+        lastName.style.border = 'solid red 2px';
+        return false;
+    } else {
+        errorLastName.style.display = 'none';
+        lastName.style.border = 'solid #279e7a 3px';
+        return true;
+    }
 }
 
 /**
@@ -136,19 +136,19 @@ function validateLastName(lastName) {
  * @returns message d'erreur
  */
 function validateEmail(eMail) {
-  if (!/[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/.test(eMail.value)) {
-    errorMail.style.display = "inline"
-    errorMail.innerText = "Veuillez entrer une adresse mail valide.";
-    errorMail.style.color = 'red';
-    errorMail.style.fontSize = '0.8rem';
-    errorMail.style.marginTop = '10px';
-    eMail.style.border = 'solid red 2px';
-    return false;
-  } else {
-    errorMail.style.display = 'none';
-    eMail.style.border = 'solid #279e7a 3px';
-    return true;
-  }
+    if (!/[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/.test(eMail.value)) {
+        errorMail.style.display = "inline"
+        errorMail.innerText = "Veuillez entrer une adresse mail valide.";
+        errorMail.style.color = 'red';
+        errorMail.style.fontSize = '0.8rem';
+        errorMail.style.marginTop = '10px';
+        eMail.style.border = 'solid red 2px';
+        return false;
+    } else {
+        errorMail.style.display = 'none';
+        eMail.style.border = 'solid #279e7a 3px';
+        return true;
+    }
 }
 
 /**
@@ -157,19 +157,19 @@ function validateEmail(eMail) {
  * @returns message d'erreur
  */
 function validateBirthdate(birthDate) {
-  if (!birthDate.value.match(/^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/)) {
-    errorBirthDate.style.display = "inline";
-    errorBirthDate.innerText = "Veuillez indiquer votre date de naissance.";
-    errorBirthDate.style.color = 'red';
-    errorBirthDate.style.fontSize = '0.8rem';
-    errorBirthDate.style.marginTop = '10px';
-    birthDate.style.border = 'solid red 2px';
-    return false;
-  } else {
-    errorBirthDate.style.display = 'none';
-    birthDate.style.border = 'solid #279e7a 3px';
-    return true;
-  };
+    if (!birthDate.value.match(/^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/)) {
+        errorBirthDate.style.display = "inline";
+        errorBirthDate.innerText = "Veuillez indiquer votre date de naissance.";
+        errorBirthDate.style.color = 'red';
+        errorBirthDate.style.fontSize = '0.8rem';
+        errorBirthDate.style.marginTop = '10px';
+        birthDate.style.border = 'solid red 2px';
+        return false;
+    } else {
+        errorBirthDate.style.display = 'none';
+        birthDate.style.border = 'solid #279e7a 3px';
+        return true;
+    };
 }
 
 /**
@@ -178,19 +178,19 @@ function validateBirthdate(birthDate) {
  * @returns message d'erreur 
  */
 function validateEventParticipation(eventParticipation) {
-  if (!eventParticipation.value.match(numbersValue)) {
-    errorEventParticipation.style.display = "inline";
-    errorEventParticipation.innerText = "Veuillez indiquer un nombre de participation à nos tournois."
-    errorEventParticipation.style.color = 'red';
-    errorEventParticipation.style.fontSize = '0.8rem';
-    errorEventParticipation.style.marginTop = '10px';
-    eventParticipation.style.border = 'solid red 2px';
-    return false;
-  } else {
-    errorEventParticipation.style.display = 'none';
-    eventParticipation.style.border = 'solid #279e7a 3px';
-    return true;
-  };
+    if (!eventParticipation.value.match(numbersValue)) {
+        errorEventParticipation.style.display = "inline";
+        errorEventParticipation.innerText = "Veuillez indiquer un nombre de participation à nos tournois."
+        errorEventParticipation.style.color = 'red';
+        errorEventParticipation.style.fontSize = '0.8rem';
+        errorEventParticipation.style.marginTop = '10px';
+        eventParticipation.style.border = 'solid red 2px';
+        return false;
+    } else {
+        errorEventParticipation.style.display = 'none';
+        eventParticipation.style.border = 'solid #279e7a 3px';
+        return true;
+    };
 }
 
 /**
@@ -199,19 +199,19 @@ function validateEventParticipation(eventParticipation) {
  * @returns message d'erreur
  */
 function validateEventCity(eventCity) {
-  // console.log(eventCity)
-  if (eventCity == undefined || eventCity == null) {
-    errorEventCity.style.display = "inline-block";
-    errorEventCity.innerText = "Veuillez choisir une ou plusieurs ville(s).";
-    errorEventCity.style.color = 'red';
-    errorEventCity.style.fontSize = '0.8rem';
-    errorEventCity.style.marginTop = '10px';
-    errorEventCity.style.width = '100%';
-    return false;
-  } else {
-    errorEventCity.style.display = 'none';
-    return true;
-  };
+    // console.log(eventCity)
+    if (eventCity == undefined || eventCity == null) {
+        errorEventCity.style.display = "inline-block";
+        errorEventCity.innerText = "Veuillez choisir une ou plusieurs ville(s).";
+        errorEventCity.style.color = 'red';
+        errorEventCity.style.fontSize = '0.8rem';
+        errorEventCity.style.marginTop = '10px';
+        errorEventCity.style.width = '100%';
+        return false;
+    } else {
+        errorEventCity.style.display = 'none';
+        return true;
+    };
 }
 
 /**
@@ -220,36 +220,36 @@ function validateEventCity(eventCity) {
  * @returns message d'erreur
  */
 function validateCgu(cgu) {
-  if (cgu.checked == false) {
-    errorCgu.style.display = "inline";
-    errorCgu.innerText = "Vous devez accepter les termes et conditions.";
-    errorCgu.style.color = 'red';
-    errorCgu.style.fontSize = '0.8rem';
-    errorCgu.style.marginTop = '10px';
-    return false;
-  } else {
-    errorCgu.style.display = 'none';
-    return true;
-  };
+    if (cgu.checked == false) {
+        errorCgu.style.display = "inline";
+        errorCgu.innerText = "Vous devez accepter les termes et conditions.";
+        errorCgu.style.color = 'red';
+        errorCgu.style.fontSize = '0.8rem';
+        errorCgu.style.marginTop = '10px';
+        return false;
+    } else {
+        errorCgu.style.display = 'none';
+        return true;
+    };
 }
 
 // Validation form 
 function validate() {
-  //déclarer une variable
-  let isFormValidate = [];
+    //déclarer une variable
+    let isFormValidate = [];
 
-  isFormValidate.push(validateFirstName(firstName));
-  isFormValidate.push(validateLastName(lastName));
-  isFormValidate.push(validateEmail(eMail));
-  isFormValidate.push(validateBirthdate(birthDate));
-  isFormValidate.push(validateEventParticipation(eventParticipation));
-  isFormValidate.push(validateEventCity(eventCity));
-  isFormValidate.push(validateCgu(cgu));
+    isFormValidate.push(validateFirstName(firstName));
+    isFormValidate.push(validateLastName(lastName));
+    isFormValidate.push(validateEmail(eMail));
+    isFormValidate.push(validateBirthdate(birthDate));
+    isFormValidate.push(validateEventParticipation(eventParticipation));
+    isFormValidate.push(validateEventCity(eventCity));
+    isFormValidate.push(validateCgu(cgu));
 
-  if (!isFormValidate.includes(false)) {
-    form.style.display = 'none';
-    confirmationValidation.style.display = 'flex';
-  }
+    if (!isFormValidate.includes(false)) {
+        form.style.display = 'none';
+        confirmationValidation.style.display = 'flex';
+    }
 }
 
 // Close form with message 
